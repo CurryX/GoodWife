e393bdc848cc91bc260df981a10f0864e6bdb618
Peng Wang <pengwang@cmu.edu>
2018-01-16 19:59:43 +0800

    Define models

--- pos/models.py
+++ pos/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Member(models.Model):
+    created_time = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=20)
+    phone = models.CharField(max_length=20)
+    balance = models.FloatField()
+    discount = models.FloatField()
+    comment = models.TextField()
+
+
+class Order(models.Model):
+    member = models.ForeignKey(Member, related_name='orders')
+    created_time = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    total_price = models.FloatField()
+    cash_paid = models.FloatField()
+    card_paid = models.FloatField()
+    discount = models.FloatField()
+    complete = models.BooleanField()
+    comment = models.TextField()
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items')
+    name = models.TextField()
+    tags = models.TextField()
+    washed = models.BooleanField()
+    picked = models.BooleanField()
+    price = models.FloatField()
+    comment = models.TextField()
