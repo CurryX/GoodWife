3699ad9fb524491ce65e10f2495c102cffa5c77c
Peng Wang <pengwang@cmu.edu>
2018-01-17 15:43:54 +0800

    Add more models

--- pos/models.py
+++ pos/models.py
@@ -5,6 +5,7 @@ class Member(models.Model):
     created_time = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=20)
+    pinyin = models.CharField(max_length=20)
     phone = models.CharField(max_length=20)
     balance = models.FloatField()
     discount = models.FloatField()
@@ -32,3 +33,14 @@ class OrderItem(models.Model):
     unit_price = models.FloatField()
     quantity = models.FloatField()
     comment = models.TextField()
+
+
+class Category(models.Model):
+    name = models.TextField()
+    single_choice = models.BooleanField()
+
+
+class Tag(models.Model):
+    category = models.ForeignKey(Category, related_name='tags')
+    name = models.TextField()
+    pinyin = models.CharField(max_length=20)
